21329ff75b4a98f36898266cfae8918883910733
Eltonite <eltoniteonblockchain@gmail.com>
2022-01-05 23:46:18 -0600

    Add tailwindcss to project

--- src/App.js
+++ src/App.js
@@ -10,13 +10,17 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link "
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
+        <br />
+        <h1 className="text-3xl font-bold rounded-lg bg-slate-500 px-3 py-3">
+          HELLO TAILWINDCSS
+        </h1>
       </header>
     </div>
   );
